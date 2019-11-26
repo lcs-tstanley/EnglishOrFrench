d7d3bd7f61f2bd3355a195f624577fad5aaf81de
Trent Stanley <tstanley@lcs.on.ca>
2019-11-26 11:16:19 -0500

    Added logic to check for s and t characters

--- EnglishOrFrench/ViewController.swift
+++ EnglishOrFrench/ViewController.swift
@@ -9,24 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    //MARK: outlets
+    
+    //MARK: Properties
     @IBOutlet weak var inputMessage: UITextField!
     @IBOutlet weak var outputResult: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
     @IBAction func analyzeButton(_ sender: Any) {
+        //Obtain value from text field
+        guard let inputMessageAsString = inputMessage.text, inputMessageAsString.count > 0 else {
+            outputResult.text = "Please enter some text or else tu est morte."
+            return
+        }
+        
+        //Make variables to check T and S count
+        var tCount = 0
+        var sCount = 0
+        
+        //Check each phrase for Ts and Ss
+        for singleCharacter in inputMessageAsString.lowercased() {
+            if singleCharacter == "s" {
+                sCount += 1
+            }
+            
+            if singleCharacter == "t" {
+                tCount += 1
+            }
+            
+            if tCount > sCount {
+                outputResult.text = "English"
+            }
+            
+            if sCount >= tCount {
+                outputResult.text = "French"
+                
+            }
+            
+            
+            
+            
+        }
         
         
         
-        
-    }
-    
-    
-
 }
 
+}
